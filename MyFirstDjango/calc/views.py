672ea149e5ba7d7793b0aa921462aa011219a9a3
Veda Kosuri <vedatrayani.kosuri@gmail.com>
2025-08-13 11:39:46 +0530

    first commit

---
+++ MyFirstDjango/calc/views.py
@@ -0,0 +1,14 @@
+
+# Create your views here.
+from django.shortcuts import render
+def home(request):
+    return render(request,'home.html',{'name':'Veda'})
+
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    val3=val1+val2
+    return render(request,'result.html',{'result':val3})
+
+def dashboard(request):
+    return render(request,'dashboard.html')
